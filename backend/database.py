6ba79c88b21c05efe6ec4909e05bdec0b29fcef7
aaroncaao <aarontac185@gmail.com>
2026-06-24 11:47:46 -0700

    finished database.py

--- backend/database.py
+++ backend/database.py
@@ -1,3 +1,13 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from dotenv import load_dotenv
 import os
+
+load_dotenv()
+databaseURL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(databaseURL, echo = True)
+async_session = async_sessionmaker(engine, expire_on_commit = False)
+
+async def get_db():
+    async with async_session() as session:
+        yield session
